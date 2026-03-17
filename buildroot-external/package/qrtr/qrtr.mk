a058fa0eac228b66d4d34bf55a941805dfbaa2e4
Marc Lainez <marc.lainez@gmail.com>
2026-03-17 07:26:28 +0100

    qrtr: Enable staging install and add license
    - Set QRTR_INSTALL_STAGING=YES so headers/libs are available for
      packages that link against libqrtr (hexagonrpc, tqftpserv)
    - Add BSD-3-Clause license declaration

--- buildroot-external/package/qrtr/qrtr.mk
+++ buildroot-external/package/qrtr/qrtr.mk
@@ -6,5 +6,7 @@
 
 QRTR_VERSION = ef44ad10f284410e2db4c4ce22c8645f988f8402
 QRTR_SITE = $(call github,linux-msm,qrtr,$(QRTR_VERSION))
+QRTR_INSTALL_STAGING = YES
+QRTR_LICENSE = BSD-3-Clause
 
 $(eval $(meson-package))
